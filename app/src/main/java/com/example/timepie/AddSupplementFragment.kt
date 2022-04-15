03e6d647682a9e2cfd99b2c824ba60f6222e3803
Alyssa Vu <alyssa130300@gmail.com>
2022-04-15 18:05:52 -0400

    Update spinner item

--- app/src/main/java/com/example/timepie/AddSupplementFragment.kt
+++ app/src/main/java/com/example/timepie/AddSupplementFragment.kt
@@ -31,7 +31,7 @@ class AddSupplementFragment : Fragment() {
         ArrayAdapter.createFromResource(
             requireContext(),
             R.array.daily_frequency_array,
-            android.R.layout.simple_spinner_item
+            R.layout.spinner_item
         ).also { adapter ->
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
